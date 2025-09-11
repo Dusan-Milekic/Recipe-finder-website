4eb2ee19e3bb15dc8c5a7414ab4e3510db2e1bf1
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-09-11 09:56:40 +0200

    Responosive added
    Need to implement burger menu

--- Recipe-Finder/src/Header.jsx
+++ Recipe-Finder/src/Header.jsx
@@ -7,6 +7,14 @@ export default function Header() {
         <div className="menu">
           <img src="/src/assets/images/icon-hamburger-menu.svg" alt="menu" />
         </div>
+        <div className="links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Recipes</a>
+        </div>
+        <div className="browse">
+          <button>Browse Recipes</button>
+        </div>
       </header>
     </>
   );
