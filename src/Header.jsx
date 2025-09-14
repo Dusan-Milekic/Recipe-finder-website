7e704ff51c83ca044a9a85754ea1d4571906dac4
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-09-14 18:06:08 +0200

    for buidl

--- src/Header.jsx
+++ src/Header.jsx
@@ -32,13 +32,13 @@ export default function Header() {
       <div className="menu-box" ref={menu_boxRef}>
         <ul>
           <li>
-            <a href="home">Home</a>
+            <a href="/home">Home</a>
           </li>
           <li>
-            <a href="about">About</a>
+            <a href="/about">About</a>
           </li>
           <li>
-            <a href="recipes">Recipes</a>
+            <a href="/recipes">Recipes</a>
           </li>
         </ul>
         <button>Browse recipes</button>
